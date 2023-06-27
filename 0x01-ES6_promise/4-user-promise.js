197323046222881b69257e9d2f131f044829b4e0
areignel <48579497+areignel@users.noreply.github.com>
2023-06-27 22:14:25 +0100

    Update 4-user-promise.js

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,9 +1,3 @@
-// 4-user-promise.js
-function signUpUser(firstName, lastName) {
-  return Promise.resolve({
-    firstName: firstName,
-    lastName: lastName,
-  });
+export default function signUpUser(firstName, lastName) {
+  return Promise.resolve({ firstName, lastName });
 }
-
-export default signUpUser;
