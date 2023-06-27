1542fd439827b32dd97a8f68818aa85560689c0a
areignel <48579497+areignel@users.noreply.github.com>
2023-06-27 21:48:44 +0100

    Create 2-then.js

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+// 2-then.js
+function handleResponseFromAPI(promise) {
+  promise
+    .then((response) => {
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'success',
+      };
+    })
+    .catch(() => new Error());
+}
+
+export default handleResponseFromAPI;
