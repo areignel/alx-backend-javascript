29b14646d790112bfe7b1cf1f95344ee2deba2cc
areignel <48579497+areignel@users.noreply.github.com>
2023-06-27 22:12:05 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,15 +1,12 @@
-// 3-all.js
-import { uploadPhoto, createUser } from "./utils";
+/* eslint-disable import/extensions */
+import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
-    .then((responses) => {
-      const [photoResponse, userResponse] = responses;
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-    })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const create = createUser();
+  return Promise.all([upload, create]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
 }
-
-export default handleProfileSignup;
